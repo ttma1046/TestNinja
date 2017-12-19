40292f6c9a6cc11518f555a12cf5686cff179d8b
Chandler Fang <chandlerfang@gmail.com>
2017-12-19 22:37:33 +1100

    Project Initial.

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,53 @@
+﻿using TestNinja.Fundamentals;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Add(1, 2);
+            
+            // Assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(2, 1);
+            
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+            
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Max_BothArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(2, 2);
+            
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+    }
+}
