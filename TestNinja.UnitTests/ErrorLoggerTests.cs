19ae9cdd529ededf36f33871fa05cb3b5c8225d5
Chandler Fang <cfang@jadeworld.com>
2018-01-03 11:53:12 +1100

    Remove.

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+            
+            logger.ErrorLogged += (sender, args) => { id = arg; };
+            
+            logger.Log("a");
+            
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+    }
+}
