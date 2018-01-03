dcb7d77099aaf88503576e646d65ba67fe0b4ac2
Chandler Fang <cfang@jadeworld.com>
2018-01-03 17:31:11 +1100

    Stack Tests.

--- TestNinja.UnitTests/StackTests.cs
+++ TestNinja.UnitTests/StackTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -8,7 +9,7 @@ namespace TestNinja.UnitTests
     {
         private Stack<object> _stack;
         
-        // SetUp
+        //  
         [SetUp]
         public void SetUp()
         {
@@ -17,11 +18,77 @@ namespace TestNinja.UnitTests
         
         [Test]
         [TestCase(null)]
-        public void Push_NullObjectPassing_ReturnArgumentNullException(object obj)
+        public void Push_NullObjectPassing_ThrowArgumentNullException(object obj)
         {
             Assert.That(() => _stack.Push(obj), Throws.ArgumentNullException);
         }
+
+        [Test]
+        [TestCase(10)]
+        public void Push_WhenCalled_AddItemIntotheList(int item)
+        {
+            var perviousCount = _stack.Count;
+            _stack.Push(item);
+            var currentCount = _stack.Count;
+            
+            Assert.That(currentCount - perviousCount, Is.EqualTo(1));
+        }
         
+        [Test]
+        [TestCase(10)]
+        public void Push_WhenCalled_AddItemIntotheListCanPeek(int item)
+        {
+            var perviousCount = _stack.Count;
+            _stack.Push(item);
+            var currentCount = _stack.Count;
+            var result = _stack.Peek();
+            
+            Assert.That(currentCount - perviousCount, Is.EqualTo(1));
+            Assert.That(result, Is.EqualTo(item));
+        }
         
+        [Test]
+        public void Pop_NothingInTheStack_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.Exception.TypeOf<InvalidOperationException>());
+        }
+        
+        [Test]
+        [TestCase(10)]
+        public void Pop_WhenCalled_ReturnLastItem(int item)
+        {
+            _stack.Push(item);
+            var result = _stack.Pop();
+            
+            Assert.That(result, Is.EqualTo(item));
+        }
+        
+        [Test]
+        [TestCase(10)]
+        public void Pop_WhenCalled_ItemRemoveFromtheList(int item)
+        {
+            _stack.Push(item);
+            var perviousCount = _stack.Count;
+            var result = _stack.Pop();
+            var currentCount = _stack.Count;
+            Assert.That(result, Is.EqualTo(item));
+            Assert.That(perviousCount - currentCount, Is.EqualTo(1));
+        }
+        
+        [Test]
+        public void Peek_NothingInTheStack_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Peek(), Throws.Exception.TypeOf<InvalidOperationException>());
+        }
+        
+        [Test]
+        [TestCase(10)]
+        public void Peek_WhenCalled_LastItemInTheListReturn(int item)
+        {
+            _stack.Push(item);
+            var result = _stack.Peek();
+            
+            Assert.That(result, Is.EqualTo(item));
+        }
     }
 }
