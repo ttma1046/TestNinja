4bd0e499865d74c0ab2385427f36df695d55b2b5
Chandler Fang <chandlerfang@gmail.com>
2018-01-03 22:44:21 +1100

    Refactoring, start doing DI.

---
+++ TestNinja.UnitTests/FakeFileReader.cs
@@ -0,0 +1,12 @@
+﻿using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    public class FakeFileReader: IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
